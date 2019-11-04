a1199777f2a24919f1a88a33c54b81797dc0821b
jimmyadkins <56000625+jimmyadkins@users.noreply.github.com>
2019-11-04 14:51:20 -0600

    fixed the dummy proof on inputs

--- src/Referee.java
+++ src/Referee.java
@@ -21,47 +21,41 @@ public class Referee {
 		while (playing) {
 			System.out.println();
 			System.out.println(myBoard.toString());
-			System.out.println("If you want to choose a set, type 'set'. If you want to quit, type 'quit'. If you want to deal 3 more cards, type 'deal'.");
-			String choice = scan.nextLine();
-			if (!choice.equals("set") || !choice.equals("quit") || !choice.equals("deal")) {
-				boolean retry = true;
-				while (retry) {
-					System.out.println("We didn't catch that, please try again.");
-					choice = scan.nextLine();
-					if (choice.equals("set") || choice.equals("quit") || choice.equals("deal")) {
-						retry = false;
+			boolean choosing = true;
+			while (choosing)
+			{
+				System.out.println("If you want to choose a set, type 'set'. If you want to quit, type 'quit'. If you want to deal 3 more cards, type 'deal'.");
+				String choice = scan.nextLine();
+				if (choice.equals("quit"))
+				{
+					playing = false;
+					break;
+				}
+				else if (choice.equals("deal"))
+				{
+					if (myBoard.getNumCardsOnBoard() < 15) {
+						System.out.println("Dealing three cards...");
+						myBoard.dealThreeCards(myDeck);
+					} else if (myBoard.getNumCardsOnBoard() == 15) {
+						System.out.println("There are already 15 cards in play!");
+					} else if (myDeck.outOfCards()) {
+						System.out.println("There are no more cards in the deck!");
+					}
+					break;
+				}
+				if (choice.equals("set")) {
+					System.out.println("Which cards make a set?");
+					int card1 = scan.nextInt() - 1;
+					int card2 = scan.nextInt() - 1;
+					int card3 = scan.nextInt() - 1;
+					if (myBoard.isLegal(card1, card2, card3)) {
+						System.out.println("Good job- that's a set!");
+						myBoard.remove3Cards(card1, card2, card3);
+						myBoard.dealThreeCards(myDeck);
+					} else {
+						System.out.println("That isn't a set! Try again.");
 					}
 				}
-			}
-			if (choice.equals("quit"))
-			{
-				playing = false;
-				break;
-			}
-			else if (choice.equals("deal"))
-			{
-				if (myBoard.getNumCardsOnBoard() < 15) {
-					System.out.println("Dealing three cards...");
-					myBoard.dealThreeCards(myDeck);
-				} else if (myBoard.getNumCardsOnBoard() == 15) {
-					System.out.println("There are already 15 cards in play!");
-				} else if (myDeck.outOfCards()) {
-					System.out.println("There are no more cards in the deck!");
-				}
-			}
-			else if (choice.equals("set")) {
-				System.out.println("Which cards make a set?");
-				int card1 = scan.nextInt() - 1;
-				int card2 = scan.nextInt() - 1;
-				int card3 = scan.nextInt() - 1;
-				if (myBoard.isLegal(card1, card2, card3)) {
-					System.out.println("Good job- that's a set!");
-					myBoard.remove3Cards(card1, card2, card3);
-					myBoard.dealThreeCards(myDeck);
-				} else {
-					System.out.println("That isn't a set! Try again.");
-				}
-
 			}
 		}
 	}
--- src/SetRunner.java
+++ src/SetRunner.java
@@ -6,31 +6,21 @@ public class SetRunner {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		ref = new Referee();
-		boolean isPlaying = true;
 		System.out.println("Hello there, welcome to the set casino where we only play set!");
-		System.out.println("Would you like to play? Type 'yes' or 'no'");
-		String choice = scan.nextLine();
+		boolean choosing = true;
+		while (choosing) {
+			System.out.println("Would you like to play? Type 'yes' or 'no'");
+			String choice = scan.nextLine();
 
-		if (!choice.equals("no")||!choice.equals("yes")) {
-			boolean retry = true;
-			while (retry)
-			{
-				System.out.println("We didn't catch that- Type 'yes' or 'no'.");
-				choice = scan.nextLine();
-				if (choice.equals("no") || choice.equals("yes"))
-				{
-					retry = false;
-				}
+			if (choice.equals("no")) {
+				System.out.println("Ok then... Bye!");
 			}
-
-		}
-
-		if (choice.equals("no")) {
-			System.out.println("Ok then... Bye!");
-			isPlaying = false;
-		}
-		if (choice.equals("yes")) {
-			ref.playGame();
+			else if (choice.equals("yes")) {
+				ref.playGame();
+			}
+			else{
+				System.out.println("Unknown input. Try again.");
+			}
 		}
 	}
 }
